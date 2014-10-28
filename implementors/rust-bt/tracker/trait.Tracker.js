e70c81f991e3f4a428729140ee8cc838ad94b3c3
Andrew Miller <amiller4421@gmail.com>
2014-10-28 12:03:06 -0700

    First Docs Commit

---
+++ implementors/rust-bt/tracker/trait.Tracker.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rust-bt'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
