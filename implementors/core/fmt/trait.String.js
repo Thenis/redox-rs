a20b6c5a5e52bb7fb0734cd36d5082e8fca33bea
travis-ci <travis@travis-ci.org>
2015-01-14 00:10:50 +0000

    Latest rustdoc on successful travis build 28 auto-pushed to gh-pages

---
+++ implementors/core/fmt/trait.String.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bittorrent'] = ["<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.String.html' title='core::fmt::String'>String</a> for <a class='enum' href='bittorrent/peer/message/enum.StateChange.html' title='bittorrent::peer::message::StateChange'>StateChange</a>","<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.String.html' title='core::fmt::String'>String</a> for <a class='enum' href='bittorrent/peer/message/enum.PeerMessage.html' title='bittorrent::peer::message::PeerMessage'>PeerMessage</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/slice/trait.AsSlice.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bittorrent'] = ["<a class='stability Unstable' title='Unstable: will be replaced by slice syntax'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/slice/trait.AsSlice.html' title='core::slice::AsSlice'>AsSlice</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.u8.html'>u8</a>&gt; for <a class='struct' href='bittorrent/peer/block/struct.Block.html' title='bittorrent::peer::block::Block'>Block</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
